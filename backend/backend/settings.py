9cbc5ceff3d8d33ee9d08dcd71451e400e64a4bf
TheDrakl <denysmelnyk262626@gmail.com>
2025-05-20 19:28:34 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,6 +112,9 @@ REST_FRAMEWORK = {
         "core.auth_backends.CookieJWTAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 500,
 }
