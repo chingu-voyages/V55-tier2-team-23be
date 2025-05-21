3f2a069bc84085b27473b1e307eb88ecfa445b81
TheDrakl <denysmelnyk262626@gmail.com>
2025-05-21 12:39:39 +0100

    Created UserRating model

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser, Resource, Tag
+from .models import CustomUser, Resource, Tag, UserRating
 
 
 admin.site.register(CustomUser)
 admin.site.register(Resource)
 admin.site.register(Tag)
+admin.site.register(UserRating)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-
+from backend.settings import AUTH_USER_MODEL
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, username, password=None, **extra_fields):
@@ -62,4 +63,16 @@ class Resource(models.Model):
     tags = models.ManyToManyField(Tag, related_name='resources')
 
     def __str__(self):
-        return f'{self.author} - {self.name}'    
+        return f'{self.author} - {self.name}'
+    
+class UserRating(models.Model): 
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): 
+        return f'{self.user} - {self.resource} - {self.rating}'
